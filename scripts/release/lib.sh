4be8e57e9e24af1a904b0c29a4c9f24cf0dbf0a1
Alexandre Karassouloff <a.karassouloff@gmail.com>
2026-09-17 19:31:55 +0200

    ci: sign and attest main images by digest

--- scripts/release/lib.sh
+++ scripts/release/lib.sh
@@ -1,4 +1,5 @@
 # shellcheck shell=bash disable=SC2034
+shopt -s inherit_errexit
 IMAGE=${IMAGE:-ghcr.io/akarasso/opencargo}
 REPO=${REPO:-akarasso/opencargo}
 ISSUER=https://token.actions.githubusercontent.com
@@ -20,4 +21,15 @@ registry_digest() {
 
 require_digest() {
   [[ $1 =~ ^sha256:[0-9a-f]{64}$ ]] || die "'$1' is not a sha256 digest"
+}
+
+# The digest docker push reports for the local image it pushed; never a registry lookup.
+push_digest() {
+  local ref=$1 tag=${1##*:} log matches
+  log=$(mktemp)
+  docker push "$ref" | tee "$log" >&2
+  matches=$(grep -E "^$tag: digest: sha256:[0-9a-f]{64} size: [0-9]+$" "$log" | sed -E 's/.* digest: (sha256:[0-9a-f]{64}) .*/\1/' || true)
+  rm -f "$log"
+  [[ -n $matches && $(wc -l <<< "$matches") -eq 1 ]] || die "expected one '$tag: digest:' line in docker push output, got '${matches//$'\n'/ }'"
+  echo "$matches"
 }
