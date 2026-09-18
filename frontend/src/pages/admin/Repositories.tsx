fabb49ef43389e80d1d57738ecbbffc5c1193885
Alexandre Karassouloff <a.karassouloff@gmail.com>
2026-09-18 17:39:54 +0200

    fix(integration): the admin UI offers pypi beside maven and nuget

--- frontend/src/pages/admin/Repositories.tsx
+++ frontend/src/pages/admin/Repositories.tsx
@@ -320,6 +320,7 @@ function RepositoriesInner() {
                 <option value="cargo">cargo</option>
                 <option value="oci">oci</option>
                 <option value="go">go</option>
+                <option value="pypi">pypi</option>
                 <option value="maven">maven</option>
                 <option value="nuget">nuget</option>
               </select>
